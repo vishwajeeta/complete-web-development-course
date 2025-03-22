6bd37d035035d3d8d50ada4b1c445950b8ee8333
vishwajeeta <gs699542@gmail.com>
2025-03-22 10:18:37 +0530

    installed ejs

---
+++ express/Note.md
@@ -0,0 +1,9 @@
+## install express
+```
+npm install express
+```
+## render html
+install virtual engine to render html `view/index.ejs`
+```
+npm i ejs
+```
--- express/app1.js
+++ express/app1.js
@@ -2,9 +2,20 @@ const Express=require("express")
 const app=Express()
 const PORT=5000
 
+app.set("view engine","ejs");
+
 app.listen(PORT,()=>{
     console.log(`http://localhost:${PORT}`)
 })
+
 app.get("/",(req,res)=>{
     res.send("<h1>Hello world</h1>")
+})
+
+app.get("/about",(req,res)=>{
+    res.send("About")
+})
+
+app.get("/more",(req,res)=>{
+    res.render("index")
 })
